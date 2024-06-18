efaed5fcda531fc66145772b15cc9d1c200102bc
codeleventh <eleventh@vechnost-eto-ne-srok.ru>
2024-06-19 01:15:38 +0400

    + map list page (frontend)

--- frontend/src/components/pages/MapListPage.tsx
+++ frontend/src/components/pages/MapListPage.tsx
@@ -1,5 +1,56 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import {Center, Loader, Notification, Table} from "@mantine/core";
+import {Edit, InfoCircle} from "tabler-icons-react";
+import {Link} from "react-router-dom";
+import {DATETIME_FORMAT, DEFAULT_FEATURE_NAME} from "../../const";
+import dayjs from "dayjs";
+import {ErrorTemplate} from "../ErrorTemplate";
+import {errorHandler} from "../utils/apiUtils";
+import {pipe, prop} from "ramda";
+import axios from 'axios';
+import {IApiResponse} from "../../model/rests";
+import {IMapMeta} from "../../model/model";
 
 export const MapListPage: React.FC = () => {
-    return <h1>TODO:</h1>
+    const [response, setResponse] = useState<IApiResponse<IMapMeta[]>>()
+
+    useEffect(() => {
+        axios.get('/api/meta')
+            .then(pipe(prop('data'), setResponse))
+            .catch(pipe(errorHandler, setResponse))
+    }, [])
+
+    return (<>
+        {!response
+            ? <div id="error-wrapper"><Center><Loader/></Center></div>
+            : !response.success
+                ? <ErrorTemplate errors={response.errors} warnings={response.warnings}/>
+                : !response.body.length
+                    ? <Notification disallowClose icon={<InfoCircle floodColor='indigo'/>}>У вас ещё нет карт.
+                        Попробуйте <Link to='/create'>создать</Link> свою первую карту.</Notification>
+                    : <><Table>
+                        <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Название</th>
+                            <th>Язык</th>
+                            <th>Создана</th>
+                            <th/>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {response.body.map((row, i) => <tr key={i}>
+                                <td><Link to={`/${row.identifier}`}>{row.identifier}</Link></td>
+                                <td>{row.title ?? DEFAULT_FEATURE_NAME}</td>
+                                <td>{row.lang}</td>
+                                <td>{dayjs(row.createdAt).format(DATETIME_FORMAT)}</td>
+                                <td>
+                                    <td><Link to={`/${row.identifier}/edit`}><Edit/></Link></td>
+                                </td>
+                            </tr>
+                        )}
+                        </tbody>
+                    </Table></>
+        }
+    </>)
 }
--- frontend/src/components/pages/MapsPage.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export const MapsPage: React.FC = () => {
-    return <h1>Cards page</h1>
-}
